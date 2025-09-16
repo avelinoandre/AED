bf20dbb6beaad4928326a65ed5847522b8259dde
avelinoandre <andreavelino002@gmail.com>
2025-09-15 21:28:39 -0300

    aa

--- Duplamente/q1.c
+++ Duplamente/q1.c
@@ -42,7 +42,6 @@ void adicionarOrdenado(Node **head, int valor) {
 
         if (*head == NULL) {
             *head = novo;
-            return;
         }
 
         Node *temp = *head;
@@ -51,7 +50,6 @@ void adicionarOrdenado(Node **head, int valor) {
             novo->prox = temp;
             temp->ant = novo;
             *head = novo;
-            return;
         }
 
         while (temp->prox != NULL && temp->prox->valor < valor) {
@@ -74,4 +72,101 @@ void adicionarOrdenado(Node **head, int valor) {
 
 
     }
+
+
+void adicionar (Node **head, int numero){
+
+    Node *novo = (Node *)malloc(sizeof(Node));
+
+    if (novo != NULL){
+
+        novo->valor = numero;
+        novo->prox = *head;
+        novo->ant = NULL;
+
+        if (*head == NULL){
+
+            *head = novo;
+
+        }else{
+
+            (*head)->ant = novo;
+
+        }
+
+    }
+
+}
+
+void AdicionarAlternado(Node **head1, Node **head2, Node **head3){
+
+    Node *temp1 = *head1;
+    Node *temp2 = *head2;
+
+    while(temp1 != NULL || temp2 != NULL){
+
+        if(temp1 != NULL){
+
+            adicionar(head3, temp1->valor);
+            temp1 = temp1->prox;
+
+        }
+
+        if(temp2 != NULL){
+
+            adicionar(head3, temp2->valor);
+            temp2 = temp2->prox;
+
+        }
+    }
+}
+
+
+
+void quantidadeDeNos(Node *head, int valor) {
+
+    if (head != NULL) {
+
+        Node *depois = head;
+        Node *antes = NULL;
+
+        while (depois != NULL) {
+
+            if (depois->valor == valor) {
+                break;
+            }
+
+            depois = depois->prox;
+
+        }
+
+
+        if (depois == NULL) {
+            return;
+        }
+
+        antes = depois;
+
+        int qntAntes = 0;
+
+        while (antes->ant != NULL) {
+            qntAntes++;
+            antes = antes->ant;
+        }
+
+        int qntDepois = 0;
+
+        while (depois->prox != NULL) {
+            qntDepois++;
+            depois = depois->prox;
+            
+        }
+
+
+        printf("\nantes: %d\n", qntAntes);
+        printf("apos: %d\n", qntDepois);
+
+        
+    }
+}
     
