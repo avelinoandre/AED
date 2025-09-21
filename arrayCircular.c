36c8b4de04608f8a04efd4d2a012c74811ede237
avelinoandre <andreavelino002@gmail.com>
2025-09-21 20:34:37 -0300

    aa

--- arrayCircular.c
+++ arrayCircular.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node{
+
+    int valor;
+    struct Node *prox;
+
+}Node;
+
 int main (){
     int tamanho;
     scanf("%d", &tamanho);
@@ -9,7 +16,7 @@ int main (){
     for (int i = 0; i < tamanho; i++){
         scanf("%d", &array[i]);
     }
-//4 3 2 1 - > 3 2 1 4
+    
     for (int j = 0; j < tamanho; j++){
         int temporaria = array[j];
         array[j] = array[j + 1];
@@ -17,3 +24,63 @@ int main (){
     }
 
 }
+
+
+void removerNoFinal(Node **head, Node **tail){
+
+    if(*head == NULL){
+
+        *tail = NULL;
+
+    }
+    if((*head)->prox == *head){
+
+        free(*head);
+        *head = NULL;
+        *tail = NULL;
+        
+        
+    }else{
+
+        Node *temp = *head;
+
+        while(temp->prox != *tail){
+
+            temp = temp->prox;
+
+        }
+
+        free(*tail);
+        *tail = temp;
+        (*tail)->prox = *head;
+
+    }
+
+}
+
+
+
+void adicionarNoFinal(Node **head, Node **tail, int valor){
+
+    Node *novo = (Node *)malloc(sizeof(Node));
+
+    if(novo != NULL){
+
+        novo->valor = valor;
+        novo->prox = *head;
+
+        if(*head == NULL){
+
+            *head = novo;
+            *tail = novo;
+            novo->prox = novo;
+
+        }else{
+
+            (*tail)->prox = novo;
+            *tail = novo;
+
+        }
+    }
+}
+
